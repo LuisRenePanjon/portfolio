cf37a0f232c3a4b774476367fb76a839a37dea03
Luis René Panjón <luchorene97@gmail.com>
2022-03-01 21:03:08 -0500

    Add projects and information about me

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -8,27 +8,13 @@ export const Skills = () => {
     <div className={Styles.iconcontainer}>
 
       <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/html-5.png" />
-        <p className={Styles.item}>HTML</p>
-      </div>
-
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/css-3.png" />
-        <p className={Styles.item}>CSS</p>
-      </div>
-
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/js.png" />
-        <p className={Styles.item}>JavaScript</p>
-      </div>
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/react.png" />
-        <p className={Styles.item}>React</p>
-      </div>
-
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/angular.png" />
-        <p className={Styles.item}>Angular</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>React</li>
+          <li>Angular</li>
+        </ul>
       </div>
     </div>
 
@@ -37,23 +23,14 @@ export const Skills = () => {
     <div className={Styles.iconcontainer}>
 
       <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/java.png" />
-        <p className={Styles.item}>Java</p>
-      </div>
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/piton.png" />
-        <p className={Styles.item}>Python</p>
-      </div>
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.spring} src="/assets/spring-boot.png" />
-        <p className={Styles.item}>Spring Boot</p>
+        <ul>
+          <li>Java</li>
+          <li>Python</li>
+          <li>Spring Boot</li>
+          <li>Django</li>
+        </ul>
       </div>
 
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/django.png" />
-        <p className={Styles.item}>Django</p>
-
-      </div>
 
     </div>
 
@@ -61,37 +38,24 @@ export const Skills = () => {
     <div className={Styles.iconcontainer}>
 
       <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/postgres.png" />
-        <p className={Styles.item}>PostgreSQL</p>
-      </div>
-
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.docker} src="/assets/mysql.png" />
-        <p className={Styles.item}>MySQL</p>
-      </div>
-
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.socialicon} src="/assets/mongo.png" />
-        <p className={Styles.item}>MongoDB</p>
-      </div>
-
-      <div className={Styles.itemcontainer}>
-        <img className={Styles.firestore} src="/assets/firestore.png" />
-        <p className={Styles.item}>Firestore</p>
+        <ul>
+          <li>PostgreSQL</li>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>Firestore</li>
+        </ul>
       </div>
 
     </div>
     <span className={Styles.title}>Tools</span>
     <div className={Styles.iconcontainer}>
 
-    <div className={Styles.itemcontainer}>
-      <img className={Styles.socialicon} src="/assets/git.png" />
-      <p className={Styles.item}>GIT</p>
-    </div>
-    <div className={Styles.itemcontainer}>
-      <img className={Styles.docker} src="/assets/docker.png" />
-      <p className={Styles.item}>Docker</p>
-    </div>
+      <div className={Styles.itemcontainer}>
+        <ul>
+          <li>Git</li>
+          <li>Docker</li>
+        </ul>
+      </div>
 
     </div>
   </>
